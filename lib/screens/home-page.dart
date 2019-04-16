21ce30a081b22322d09594fed8b8c8729019bc66
若林勇汰 <yufo.yuta0520@gmail.com>
2019-04-16 18:00:28 +0900

    [update] search component

--- lib/screens/home-page.dart
+++ lib/screens/home-page.dart
@@ -12,23 +12,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.widget.title),
+        elevation: 0.0,
       ),
-      body: new Center(
-        child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          children: <Widget>[
-            new TextField(
-              decoration: const InputDecoration(
-                hintText: 'Hakodate',
-                labelText: 'Search',
-              ),
-              maxLines: 1,
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+              top: 12.0,
+              bottom: 12.0,
+              left: 8.0,
+              right: 8.0,
             ),
-          ],
-        ),
+            child: TextField(
+              onChanged: (text) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                hintText: "Search",
+                border: InputBorder.none,
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.orange[600])),
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 24.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
